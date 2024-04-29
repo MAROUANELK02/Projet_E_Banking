26ec43752b215e62dcb6ffa4bf45ca03dac7ac7e
Marouane LASMAK <116379216+MAROUANELK02@users.noreply.github.com>
2024-04-29 14:53:33 +0100

    v1.0

---
+++ E_Banking_Backend/src/main/java/com/barbertime/e_banking_backend/EBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.barbertime.e_banking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBankingBackendApplication.class, args);
+    }
+
+}
---
+++ E_Banking_Backend/src/test/java/com/barbertime/e_banking_backend/EBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbertime.e_banking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
