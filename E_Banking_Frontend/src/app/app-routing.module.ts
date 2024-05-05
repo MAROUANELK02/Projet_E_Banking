9d4552b47e8c3a7a8fe578e57cb7adf905127ef2
Marouane LASMAK <116379216+MAROUANELK02@users.noreply.github.com>
2024-05-05 17:56:29 +0100

    frontend

--- E_Banking_Frontend/src/app/app-routing.module.ts
+++ E_Banking_Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {CurrentAccountsComponent} from "./current-accounts/current-accounts.component";
+import {UsersComponent} from "./users/users.component";
+import {AuthenticationGuard} from "./guards/authentication.guard";
+import {AdminModeratorAuthorizationGuard} from "./guards/admin-moderator-authorization.guard";
+import {SavingAccountsComponent} from "./saving-accounts/saving-accounts.component";
+import {OperationsComponent} from "./operations/operations.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "login", component: LoginComponent},
+  {path: "accounts/currentAccounts", component: CurrentAccountsComponent, canActivate : [AuthenticationGuard, AdminModeratorAuthorizationGuard]},
+  {path: "accounts/savingAccounts", component: SavingAccountsComponent, canActivate : [AuthenticationGuard, AdminModeratorAuthorizationGuard] },
+  {path: "operations", component: OperationsComponent, canActivate: [AuthenticationGuard, AdminModeratorAuthorizationGuard] },
+  {path: "users", canActivate: [AuthenticationGuard, AdminModeratorAuthorizationGuard] , component: UsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- E_Banking_Frontend/src/app/app.module.ts
+++ E_Banking_Frontend/src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { CurrentAccountsComponent } from './current-accounts/current-accounts.component';
+import { UsersComponent } from './users/users.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpAppInterceptor} from "./services/http-app.interceptor";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
+import { SavingAccountsComponent } from './saving-accounts/saving-accounts.component';
+import { OperationsComponent } from './operations/operations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    CurrentAccountsComponent,
+    UsersComponent,
+    SavingAccountsComponent,
+    OperationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIcon,
+    MatButton
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: HttpAppInterceptor, multi: true},
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
