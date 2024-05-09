47dbce5b80f384718a36044175c3fbd301147838
Marouane LASMAK <116379216+MAROUANELK02@users.noreply.github.com>
2024-05-09 22:56:24 +0100

    frontend

--- E_Banking_Frontend/src/app/saving-accounts/saving-accounts.component.ts
+++ E_Banking_Frontend/src/app/saving-accounts/saving-accounts.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {AppStateService} from "../services/app-state.service";
 import {SavingAccountsRepositoryService} from "../services/saving-accounts.repository.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {OperationsRepositoryService} from "../services/operations.repository.service";
+import {Operation} from "../models/operation.model";
 
 @Component({
   selector: 'app-saving-accounts',
@@ -8,7 +11,13 @@ import {SavingAccountsRepositoryService} from "../services/saving-accounts.repos
   styleUrl: './saving-accounts.component.css'
 })
 export class SavingAccountsComponent {
+  openModal!: boolean;
+  type!: string;
+  form!: FormGroup;
+
   constructor(private savingAccountsService : SavingAccountsRepositoryService,
+              private operationsService : OperationsRepositoryService,
+              private fb : FormBuilder,
               public appState : AppStateService) {
   }
 
@@ -18,6 +27,12 @@ export class SavingAccountsComponent {
 
   ngOnInit(): void {
     this.searchSavingAccounts();
+    this.form = this.fb.group({
+      accountId : this.fb.control(""),
+      firstName : this.fb.control(""),
+      lastName : this.fb.control(""),
+      amount : this.fb.control(""),
+    });
   }
 
   goToPage(number: number) {
@@ -25,20 +40,66 @@ export class SavingAccountsComponent {
     this.searchSavingAccounts();
   }
 
-  changeStatus(id: number) {
-
+  changeStatus(id: number, status: string) {
+    if(status.includes('ACTIVATED')) {
+      this.savingAccountsService.suspendAccount(id).subscribe(() => {
+        this.searchSavingAccounts();
+      });
+    }else if(status.includes('SUSPENDED')) {
+      this.savingAccountsService.activeAccount(id).subscribe(() => {
+        this.searchSavingAccounts();
+      });
+    }
   }
 
   closeAccount(id : number) {
-
+    this.savingAccountsService.closeAccount(id).subscribe(() => {
+      this.searchSavingAccounts();
+    });
   }
 
-  debitAccount(id : number) {
-
+  debitAccount(id: number, firstName: any, lastName: any) {
+    this.form.patchValue({
+      accountId : id,
+      firstName : firstName,
+      lastName : lastName
+    });
+    this.type = "debit";
+    this.openModal = true;
   }
 
-  creditAccount(id : number) {
-
+  creditAccount(id: number, firstName: any, lastName: any) {
+    this.form.patchValue({
+      accountId : id,
+      firstName : firstName,
+      lastName : lastName
+    });
+    this.type = "credit";
+    this.openModal = true;
   }
 
+  closeModal() {
+    this.type = "";
+    this.openModal = false;
+  }
+
+  saveOperation() {
+    let operation : Operation = new Operation();
+    operation.amount = this.form.value.amount;
+    operation.accountId = this.form.value.accountId;
+    if(this.type === "debit") {
+      this.operationsService.debitAccount(operation).subscribe(() => {
+        this.closeModal();
+        this.searchSavingAccounts();
+      });
+    } else if(this.type === "credit") {
+      this.operationsService.creditAccount(operation).subscribe(() => {
+        this.closeModal();
+        this.searchSavingAccounts();
+      });
+    }
+    this.form.patchValue({
+      amount : ""
+    });
+  }
 }
--- E_Banking_Frontend/src/app/services/saving-accounts.repository.service.ts
+++ E_Banking_Frontend/src/app/services/saving-accounts.repository.service.ts
@@ -3,37 +3,38 @@ import {HttpClient} from "@angular/common/http";
 import {AppStateService} from "./app-state.service";
 import {ApiResponse} from "../models/api-response.model";
 import {SavingAccount} from "../models/saving-account.model";
+import {Observable, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SavingAccountsRepositoryService {
 
-  host : string = "http://localhost:5000/api/accounts/";
+  host: string = "http://localhost:5000/api/accounts/";
 
-  constructor(private http : HttpClient, private appState : AppStateService) {
+  constructor(private http: HttpClient, private appState: AppStateService) {
   }
 
   searchSavingAccounts({
-                          currentPage = this.appState.savingAccountsState.currentPage,
-                          size = this.appState.savingAccountsState.pageSize
-                        }){
+                         currentPage = this.appState.savingAccountsState.currentPage,
+                         size = this.appState.savingAccountsState.pageSize
+                       }) {
     this.appState.savingAccountsState.status = "loading";
-    this.http.get<ApiResponse<SavingAccount>>(this.host+"saving" , {
-      params : {
-        page : currentPage,
-        size : size
+    this.http.get<ApiResponse<SavingAccount>>(this.host + "saving", {
+      params: {
+        page: currentPage,
+        size: size
       },
     }).subscribe({
-      next : (resp : ApiResponse<SavingAccount>)=>{
+      next: (resp: ApiResponse<SavingAccount>) => {
         let savingAccounts = resp.content;
         let totalPages = resp.totalPages;
         this.appState.setSavingAccountsState({
-          savingAccounts, totalPages, currentPage, status:"LOADED", errorMessage:""
+          savingAccounts, totalPages, currentPage, status: "LOADED", errorMessage: ""
         })
       },
-      error : (err)=>{
-        this.appState.setSavingAccountsState({status:"ERROR", errorMessage:err.statusText});
+      error: (err) => {
+        this.appState.setSavingAccountsState({status: "ERROR", errorMessage: err.statusText});
       }
     });
 
@@ -43,16 +44,61 @@ export class SavingAccountsRepositoryService {
     this.searchSavingAccounts({});
   }
 
-  saveAccount(savingAccount: SavingAccount, customerId : number) {
+  saveAccount(savingAccount: SavingAccount, customerId: number) {
     this.appState.savingAccountsState.status = "LOADING";
-    this.http.post<SavingAccount>(this.host+"create/savingAccount/"+customerId, savingAccount).subscribe({
-      next : ()=>{
-        this.appState.setSavingAccountsState({status:"SUCCESS", errorMessage:""});
+    this.http.post<SavingAccount>(this.host + "create/savingAccount/" + customerId, savingAccount).subscribe({
+      next: () => {
+        this.appState.setSavingAccountsState({status: "SUCCESS", errorMessage: ""});
         this.searchSavingAccounts({});
       },
-      error : (err)=>{
-        this.appState.setSavingAccountsState({status:"ERROR", errorMessage:err.statusText});
+      error: (err) => {
+        this.appState.setSavingAccountsState({status: "ERROR", errorMessage: err.statusText});
       }
     });
+  }
+
+  activeAccount(accountId: number): Observable<SavingAccount> {
+    this.appState.savingAccountsState.status = "LOADING";
+    return this.http.put<SavingAccount>(this.host + accountId + "/activated", {}).pipe(
+      tap({
+        next: () => {
+          this.appState.setSavingAccountsState({status: "SUCCESS", errorMessage: ""});
+          this.searchSavingAccounts({});
+        },
+        error: (err) => {
+          this.appState.setSavingAccountsState({status: "ERROR", errorMessage: err.statusText});
+        }
+      })
+    );
+  }
+
+  suspendAccount(accountId: number): Observable<SavingAccount> {
+    this.appState.savingAccountsState.status = "LOADING";
+    return this.http.put<SavingAccount>(this.host + accountId + "/suspended", {}).pipe(
+      tap({
+        next: () => {
+          this.appState.setSavingAccountsState({status: "SUCCESS", errorMessage: ""});
+          this.searchSavingAccounts({});
+        },
+        error: (err) => {
+          this.appState.setSavingAccountsState({status: "ERROR", errorMessage: err.statusText});
+        }
+      })
+    );
+  }
+
+  closeAccount(accountId: number): Observable<SavingAccount> {
+    this.appState.savingAccountsState.status = "LOADING";
+    return this.http.delete<SavingAccount>(this.host + accountId, {}).pipe(
+      tap({
+        next: () => {
+          this.appState.setSavingAccountsState({status: "SUCCESS", errorMessage: ""});
+          this.searchSavingAccounts({});
+        },
+        error: (err) => {
+          this.appState.setSavingAccountsState({status: "ERROR", errorMessage: err.statusText});
+        }
+      })
+    );
   }
 }
